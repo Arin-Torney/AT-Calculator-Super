d6aa20b5377ed3fa06121b512460f6695d495dd9
Arin-Torney <72132491+Arin-Torney@users.noreply.github.com>
2020-11-16 15:22:49 +0530

    Update atcalcsuper10.cpp

--- atcalcsuper10.cpp
+++ atcalcsuper10.cpp
@@ -7,9 +7,11 @@ int main() {
   long double x, y, z;
   string q;
   cout << "AT Calculator Super\nEnter which mathematical operation is to be performed. For a list of all, please type \"IDK\".\n";
+  ant:
   getline(cin, q);
   if (q == "IDK") {
-    cout << "Add\tAddition\nMinus\tSubtraction\nProd\tMultiplication\nDiv\tDivision\nHypot\tHypotenuse\nPow\tExponent\nRt\tRoot\nLog\tLogarithm\nSin\tSine\nCos\tCosine\nTan\tTangent\nAtan\tInverse Tangent\nPi\tPi\nE\tNapier's Constant(also called Euler's Number)\nPhi\tGolden Ratio\n";
+    cout << "Add\tAddition\nMinus\tSubtraction\nProd\tMultiplication\nDiv\tDivision\nHypot\tHypotenuse\nPow\tExponent\nRt\tRoot\nLog\tLogarithm\nSin\tSine\nCos\tCosine\nTan\tTangent\nAtan\tInverse Tangent\nPi\tPi\nE\tNapier's Constant(also called Euler's Number)\nPhi\tGolden Ratio\nNow enter your mathematical operation code.\n";
+    goto ant;
   }
   getch();
   return 0;
