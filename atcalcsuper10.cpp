f4107161050896cc2a39b053b7612692c45819d7
Arin-Torney <72132491+Arin-Torney@users.noreply.github.com>
2020-11-16 15:13:30 +0530

    Update atcalcsuper10.cpp

--- atcalcsuper10.cpp
+++ atcalcsuper10.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+using namespace std;
 
+int main() {
+  long double x, y, z;
+  string q;
+  cout << "AT Calculator Super\nEnter which mathematical operation is to be performed. For a list of all, please type \"IDK\".\n";
+  getline(cin, q);
+  return 0;
+}
