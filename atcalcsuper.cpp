7c9ac6d21c820d500598efe00b92b7e9a1d2f498
Arin-Torney <72132491+Arin-Torney@users.noreply.github.com>
2020-11-18 17:38:44 +0530

    Update atcalcsuper.cpp

--- atcalcsuper.cpp
+++ atcalcsuper.cpp
@@ -21,10 +21,10 @@ int main() {
   ant:
   getline(cin, q);
   if (q == "IDK") {
-    cout << "Add\tAddition\nMinus\tSubtraction\nProd\tMultiplication\nDiv\tDivision\nHypot\tHypotenuse\nPow\tExponent\nLog\tNatural Logarithm\nSin\tSine\nCos\tCosine\nTan\tTangent\nAtan\tInverse Tangent\nPi\tPi\nEn\tNapier's Constant(also called Euler's Number)\nPhi\tGolden Ratio\nNow enter your mathematical operation or constant value code.\n";
+    cout << "Add\tAddition\nMinus\tSubtraction\nProd\tMultiplication\nDiv\tDivision\nHypot\tHypotenuse\nPow\tExponent\nGmean\tGeometric Mean\nWmean\tWeighted Mean with Default Weights\nLog\tNatural Logarithm\nSin\tSine\nCos\tCosine\nTan\tTangent\nAtan\tInverse Tangent\nPi\tPi\nEn\tNapier's Constant(also called Euler's Number)\nPhi\tGolden Ratio\nNow enter your mathematical operation or constant value code.\n";
     goto ant;
   }
-  else if ((q == "Add")||(q == "Subtract")||(q == "Prod")||(q == "Div")||(q == "Hypot")||(q == "Pow")) {
+  else if ((q == "Add")||(q == "Subtract")||(q == "Prod")||(q == "Div")||(q == "Hypot")||(q == "Pow")||(q == "Gmean")||(q == "Wmean")) {
     long double y;
     cout << "Enter first number.(decimals are allowed as input)\n";
     cin >> x;
@@ -45,6 +45,15 @@ int main() {
     else if (q == "Pow") {
       z = pow(x, y);
     }
+    else if (q == "Gmean") {
+      z = x * y;
+      z = pow(z, 0.5);
+    }
+    else if (q == "Wmean") {
+      x = x * 0.7;
+      y = y * 0.3;
+      z = x + y;
+      z /= 2;
   }
   else if((q == "Sin")||(q == "Cos")||(q == "Tan")||(q == "Atan")||(q == "Log")) {
     cout << "Enter one number.(decimals are allowed as input)";
